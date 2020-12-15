cccbc9fce3d379b7a899f01697470c7713505b95
Gongda Lu <37665011+GongdaLu@users.noreply.github.com>
2020-12-15 13:49:42 +0000

    Update 5.0_R_within_Python.py

--- 5.0_R_within_Python.py
+++ 5.0_R_within_Python.py
@@ -1,3 +1,17 @@
+
+# You can use R within Python under the Jupyter Notebook IDE.
+# Sometimes this can be useful, as some very useful packages are currently written in R 
+# (openair to download surface data in UK, rmweather package and some statistics)
+
+
+
+
+
+
+
+
+
+# intructions to enable R in Jupyter Notebook
 https://anderfernandez.com/en/blog/how-to-program-with-python-and-r-in-the-same-jupyter-notebook/
 
 https://github.com/ipython-books/cookbook-2nd-code/blob/master/chapter07_stats/08_r.ipynb
