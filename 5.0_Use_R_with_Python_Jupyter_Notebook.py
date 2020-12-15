3c25b613f7db2fd871995a2b929474bcebdaa8c3
Gongda Lu <37665011+GongdaLu@users.noreply.github.com>
2020-12-15 13:35:37 +0000

    Create 5.0_Use_R_with_Python_Jupyter_Notebook.py

---
+++ 5.0_Use_R_with_Python_Jupyter_Notebook.py
@@ -0,0 +1,3 @@
+https://anderfernandez.com/en/blog/how-to-program-with-python-and-r-in-the-same-jupyter-notebook/
+
+https://github.com/ipython-books/cookbook-2nd-code/blob/master/chapter07_stats/08_r.ipynb
