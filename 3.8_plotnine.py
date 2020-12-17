ba9685ad8b78a4d02507dd7f5ffbea306b4ebd70
Gongda Lu <37665011+GongdaLu@users.noreply.github.com>
2020-12-17 22:02:23 +0000

    Create 3.8_plotnine.py

---
+++ 3.8_plotnine.py
@@ -0,0 +1,9 @@
+##############################################################################################
+##############################################################################################
+# "plotnine": A Grammar of Graphics for Python 
+# "plotnine" is almost a Python clone of "ggplot2", the most powerful data visualisation R package
+# so the syntax here is actually R programming, although in a Python package
+
+# homepage: https://plotnine.readthedocs.io/en/stable/
+
+https://datacarpentry.org/python-ecology-lesson/07-visualization-ggplot-python/index.html
