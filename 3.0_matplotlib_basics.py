9f6a4d6aa684cd2575c2cce62b059f5baf8b64fc
Gongda Lu <37665011+GongdaLu@users.noreply.github.com>
2020-12-18 22:08:44 +0000

    Update 3.0_matplotlib_basics.py

--- 3.0_matplotlib_basics.py
+++ 3.0_matplotlib_basics.py
@@ -1,1 +1,9 @@
+# quickly go through these references while polishing the figures in your manuscript
+# also utilize those Python cheatsheets
 
+https://www.matplotlib.org.cn/tutorials/#%E5%BA%8F%E8%A8%80
+https://github.com/weidafeng/python-matplotlib-practices
+https://www.cnblogs.com/devilmaycry812839668/tag/matplotlib%28Python%29/
+https://github.com/rougier/matplotlib-tutorial#introduction
+https://github.com/matplotlib/cheatsheets
+https://github.com/jbmouret/matplotlib_for_papers
