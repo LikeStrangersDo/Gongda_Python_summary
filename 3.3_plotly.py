8c2e9f58880150e89170a9701c76cc6389d40c2b
Gongda Lu <37665011+GongdaLu@users.noreply.github.com>
2020-12-20 19:15:10 +0000

    Update 3.3_plotly.py

--- 3.3_plotly.py
+++ 3.3_plotly.py
@@ -5,7 +5,7 @@
 # Advantages of interactive plots:
 # 1> You can save interactive plots as html files to carry extra information to help communicating your research.
 #    When presenting via online platforms like "Zoom", open all html files in advance. Then switch between your slides and the html tabs using functions like "New Share".
-# 2> You can still output a few static images from a single interactive plot.
+# 2> You can also output multiple static images from a single interactive plot.
 
 # How "plotly" caught my eye: You can just use the cursor or codes to zoom in to see details of a plot. 
 #                             Imagine that you have to check multiple parts on a timeseries plot of hourly data between 2013-2020.
