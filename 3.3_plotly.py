d3551ba4fe96bf6a0d4c8fa9b9e28aede58ffcf2
Gongda Lu <37665011+GongdaLu@users.noreply.github.com>
2020-12-20 18:38:47 +0000

    Update 3.3_plotly.py

--- 3.3_plotly.py
+++ 3.3_plotly.py
@@ -1,1 +1,5 @@
-# save plotly plots as an html file, use hyperlink to the html file in ppt to show the plotly plot
+
+
+# save interactive plots as an html file
+# when present using online platforms like "Zoom", open all html files before the start of the presentation
+# then just need to switch the tabs using "New Share" during the presentation
