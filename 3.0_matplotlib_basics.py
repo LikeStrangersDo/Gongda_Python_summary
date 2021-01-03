1fb7f10abfd009f59ea4f08413e8b0a7ddb82a80
Gongda Lu <37665011+GongdaLu@users.noreply.github.com>
2021-01-03 16:19:48 +0000

    Update 3.0_matplotlib_basics.py

--- 3.0_matplotlib_basics.py
+++ 3.0_matplotlib_basics.py
@@ -1,9 +1,36 @@
-# quickly go through these references while polishing the figures in your manuscript
-# also utilize those Python cheatsheets
+##############################################################################################
+##############################################################################################
+# Data visualisation with "matplotlib"
 
+# Motivation:
+# "matplotlib" is the traditional plotting tool in Python, comparing to modern tools like "seaborn", "plotly" and "bokeh", you may find "matplotlitb" plots a bit boring.
+# But "matplotlib" was initially designed for making publishable figures and has been very mature. 
+# Some very useful packages used in our research are actually built upon this (e.g. "basemap", "cartopy").
+# While mordern plotting tools focus more on interactive plots or Front-end web development.
+
+# Getting started:
+# There are two usage patterns of "matplotlib": 
+# 1> the pyplot API 
+# 2> the object-oriented API
+
+# In this basic session, I am using the "pyplot" API.
+##############################################################################################
+
+
+
+
+
+
+
+
+
+# References (summarize cheatsheets pdf also)
 https://www.matplotlib.org.cn/tutorials/#%E5%BA%8F%E8%A8%80
 https://github.com/weidafeng/python-matplotlib-practices
 https://www.cnblogs.com/devilmaycry812839668/tag/matplotlib%28Python%29/
 https://github.com/rougier/matplotlib-tutorial#introduction
 https://github.com/matplotlib/cheatsheets
 https://github.com/jbmouret/matplotlib_for_papers
+
+# End
+##############################################################################################
