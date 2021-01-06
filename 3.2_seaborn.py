839aea9fc0563b6867abd217e62dffb8c0d78188
Gongda Lu <37665011+GongdaLu@users.noreply.github.com>
2021-01-06 18:11:07 +0000

    Update 3.2_seaborn.py

--- 3.2_seaborn.py
+++ 3.2_seaborn.py
@@ -1,1 +1,10 @@
+# "Seaborn" is a package for statistical data visualisation built upon "Matplotlib".
+
+
 https://github.com/derekbanas/seaborn/blob/master/SB%20Tut.ipynb
+
+  
+  
+  
+  
+  # Another Python package for statistical data visualisation is "Altair"
