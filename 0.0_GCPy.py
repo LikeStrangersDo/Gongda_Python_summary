5e1b093b219eb532d1691eec72c1d1ff93ada92c
Gongda Lu <37665011+GongdaLu@users.noreply.github.com>
2021-01-11 11:37:02 +0000

    Create 0.0_GCPy.py

---
+++ 0.0_GCPy.py
@@ -0,0 +1,6 @@
+# learn GCPy when it is a good time:
+
+# homepage: https://gcpy.readthedocs.io/en/stable/
+# github page: https://github.com/geoschem/gcpy
+
+# maybe you can use GCpy as a sanity check for your current plotting schemes
