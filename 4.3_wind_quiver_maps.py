b7c18ed6af88c763330bd64359809605eafef908
Gongda Lu <37665011+GongdaLu@users.noreply.github.com>
2021-01-25 14:17:11 +0000

    Update 4.3_wind_quiver_maps.py

--- 4.3_wind_quiver_maps.py
+++ 4.3_wind_quiver_maps.py
@@ -1,1 +1,13 @@
+###########################################################################################################################
+###########################################################################################################################
+# Quiver plots
 
+# maybe just decide a starting point and the ending point, then the quiver can draw it directly
+# just create a df (lat,lon,v1,v2,lat_end,lon_end), then it can plot
+# the length of the arrow does not really matter as long as they are consistent in unit
+# https://pythonforundergradengineers.com/quiver-plot-with-matplotlib-and-jupyter-notebooks.html
+# https://problemsolvingwithpython.com/06-Plotting-with-Matplotlib/06.15-Quiver-and-Stream-Plots/
+# https://stackoverflow.com/questions/32271278/plot-wind-vectors-with-quiver
+
+# if you still can not plot, search "vector plot" instead
+# https://www.dkrz.de/up/services/analysis/visualization/sw/python-matplotlib/matplotlib-sourcecode/python-matplotlib-example-vector-plot
