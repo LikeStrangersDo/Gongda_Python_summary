f1385b061ea2e10e61893b33694b6a68a9b352c6
Gongda Lu <37665011+GongdaLu@users.noreply.github.com>
2021-04-12 15:29:46 +0100

    Update 0.1_flow_control.py

--- 0.1_flow_control.py
+++ 0.1_flow_control.py
@@ -1,1 +1,44 @@
 # for loop, if else, while loop, try except continue catch ... break ... based on the frequency
+
+
+
+
+import numpy as np
+import pandas as pd
+import os
+
+
+os.chdir("/rds/projects/2018/maraisea-glu-01/Study/Research_Data/BTH/2_results")
+
+df = pd.read_csv("NCP_surface_AW2016_AW2017_raw_data.csv")
+df.head()
+
+
+import time
+
+start = time.time()
+
+for i in range(df.shape[0]):
+    if ((df['lon'][i] > 114.5) &
+        (df['lon'][i] < 115) &
+        (df['lat'][i] > 35)  &
+        (df['lat'][i] < 40)):
+        print(df.lon[i])
+        
+end = time.time()
+print("TIME",end - start)
+
+
+import time
+
+start = time.time()
+
+for i in range(df.shape[0]):
+    if ((df['lon'][i] > 114.5) and
+        (df['lon'][i] < 115) and
+        (df['lat'][i] > 35)  and
+        (df['lat'][i] < 40)):
+        print(df.lon[i])
+        
+end = time.time()
+print("TIME",end - start)
