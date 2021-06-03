093435d16c62777f24f7c785a2df7d3fba012baf
Gongda Lu <37665011+GongdaLu@users.noreply.github.com>
2021-06-03 16:00:25 +0100

    Update and rename 3.7_other_maps to 3.7_other_maps.py

---
+++ 3.7_other_maps.py
@@ -0,0 +1,4 @@
+# reivew other potientially useful packages
+
+# add things like geo-plot, as it is also mentioned in Matplotlib
+# also mention this (basemap/cartopy/geoplot) in the matplotlib part
