17e860dd3da121a27e6263debf8f27f7e86b9e29
Gongda Lu <37665011+GongdaLu@users.noreply.github.com>
2021-06-03 22:38:58 +0100

    Delete 3.7_other_maps.py

--- 3.7_other_maps.py
+++
@@ -1,4 +0,0 @@
-# reivew other potientially useful packages
-
-# add things like geo-plot, as it is also mentioned in Matplotlib
-# also mention this (basemap/cartopy/geoplot) in the matplotlib part
