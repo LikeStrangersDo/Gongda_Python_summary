0b394040ac923895360d2071a3eabb48893435d4
Gongda Lu <37665011+GongdaLu@users.noreply.github.com>
2021-06-03 15:57:04 +0100

    Update 4.0_xarray_for_model_outputs.py

--- 4.0_xarray_for_model_outputs.py
+++ 4.0_xarray_for_model_outputs.py
@@ -4,3 +4,9 @@
 #
 #
 #
+# learn GCPy when it is a good time:
+
+# homepage: https://gcpy.readthedocs.io/en/stable/
+# github page: https://github.com/geoschem/gcpy
+
+# maybe you can use GCpy as a sanity check for your current plotting schemes
