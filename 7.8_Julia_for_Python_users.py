f385e099db9b49f0e94ec080b3aafadbc9d2588e
Gongda Lu <37665011+GongdaLu@users.noreply.github.com>
2021-06-04 10:10:07 +0100

    Delete 7.8_Julia_for_Python_users.py

--- 7.8_Julia_for_Python_users.py
+++
@@ -1,6 +0,0 @@
-# For Python users, it is easy to pick up Julia
-# Work on this and expect to have a Julia page in some time!
-
-https://towardsdatascience.com/how-to-learn-julia-when-you-already-know-python-641ed02b3fa7
-  
-https://sunscrapers.com/blog/the-quickest-introduction-to-julia-for-pythonistas/
