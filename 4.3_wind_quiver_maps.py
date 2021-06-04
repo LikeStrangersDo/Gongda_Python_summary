06432cdbe8c3954d1e2e8b07a3715ce0a36f87ab
Gongda Lu <37665011+GongdaLu@users.noreply.github.com>
2021-06-04 14:25:48 +0100

    plot wind directions, wind speed or just arrows

--- 4.3_wind_quiver_maps.py
+++ 4.3_wind_quiver_maps.py
@@ -2,12 +2,22 @@
 ###########################################################################################################################
 # Quiver plots
 
-# maybe just decide a starting point and the ending point, then the quiver can draw it directly
-# just create a df (lat,lon,v1,v2,lat_end,lon_end), then it can plot
-# the length of the arrow does not really matter as long as they are consistent in unit
-# https://pythonforundergradengineers.com/quiver-plot-with-matplotlib-and-jupyter-notebooks.html
-# https://problemsolvingwithpython.com/06-Plotting-with-Matplotlib/06.15-Quiver-and-Stream-Plots/
-# https://stackoverflow.com/questions/32271278/plot-wind-vectors-with-quiver
+# Wind quiver plots are normally seen in papers investigating meterological changes, climate variablities or regional pollution episodes.
+# The arrows indicate the wind directions and the length of arrows indicate wind speeds.
 
-# if you still can not plot, search "vector plot" instead
-# https://www.dkrz.de/up/services/analysis/visualization/sw/python-matplotlib/matplotlib-sourcecode/python-matplotlib-example-vector-plot
+# There are existing solutions to these kind of plots: 
+# 1> Iris: https://scitools.org.uk/iris/docs/v2.2/examples/Meteorology/wind_speed.html
+# 2> Basemap: https://basemaptutorial.readthedocs.io/en/latest/plotting_data.html#quiver
+# 3> Matplotlib: https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.quiver.html
+# 4> Matplotlib: https://pythonforundergradengineers.com/quiver-plot-with-matplotlib-and-jupyter-notebooks.html
+# 5> Matplotlib: https://problemsolvingwithpython.com/06-Plotting-with-Matplotlib/06.15-Quiver-and-Stream-Plots/
+# 6> Matplotlib: https://www.dkrz.de/up/services/analysis/visualization/sw/python-matplotlib/matplotlib-sourcecode/python-matplotlib-example-vector-plot
+
+# You can also try to create your own routine
+# maybe just decide a starting point and the ending point, create a df (lat,lon,v1,v2,lat_end,lon_end), then it can plot
+# the length of the arrow does not really matter as long as they are consistent in unit
+
+# And apart from plotting wind arrows, you can also use this kind of technique to add any arrows onto your maps or figures when you need to highlight something.
+
+# End
+###########################################################################################################################
